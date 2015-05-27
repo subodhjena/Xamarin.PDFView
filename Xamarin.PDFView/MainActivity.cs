c39c5ba9dd137a15db3ddf85bac0b78cf290009e
Subodh Jena <jenasubodh@gmail.com>
2015-05-27 16:09:16 +0530

    Fixed to load the PDF from Local Storage
    Fixed to load the PDF from Local Storage

--- Xamarin.PDFView/MainActivity.cs
+++ Xamarin.PDFView/MainActivity.cs
@@ -11,75 +11,56 @@ using Android.OS;
 
 namespace Xamarin.PDFView
 {
-	[Activity (Label = "Xamarin.PDFView", MainLauncher = true, Icon = "@drawable/icon")]
+	[Activity (Label = "PDFView", MainLauncher = true, Icon = "@drawable/icon")]
 
 	public class MainActivity : Activity
 	{
-		private WebView webView;
+		private  string _documentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+		private  string _pdfPath;
+		private  string _pdfFileName = "ThePDFDocumentName.pdf";
+		private  string _pdfFilePath;
+		private WebView _webView;
 
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
-		
-			string[] PDFViewerFileArray = { "compatibility.js", "customview.js", "data.json", "index.html", "minimal.css", "pdf.js", "pdf.worker.js", "pdffile.js", "samplePDF.pdf"};
-			var documentsPath = System.Environment.GetFolderPath (System.Environment.SpecialFolder.MyDocuments);
-			var pdfViewerPath = documentsPath + "/PDFViewer";
 
-			// Create the SubFolder id it doesn't exist
-			if (!System.IO.Directory.Exists (pdfViewerPath))
-				System.IO.Directory.CreateDirectory (pdfViewerPath);
-			else
-				Console.WriteLine ("The Directory Exists !! @ Path : "+ pdfViewerPath);
+			_pdfPath = _documentsPath + "/PDFView";
+			_pdfFilePath  = Path.Combine(_pdfPath, _pdfFileName);
 
-
-			// Enumerate through the assets and copy them to the Folder
-			foreach (string fileName in PDFViewerFileArray) {
-
-				var assetsFilePath = @"pdfviewer/" + fileName;
-				var newFilePath = pdfViewerPath + @"/" + fileName;
-
-				//Read the data from assets folder
-				var data = ApplicationContext.Assets.Open (assetsFilePath);
-				StreamReader stream = new StreamReader (data);
-				var textualData = stream.ReadToEnd ();
-
-				// Now that we have the data lets write it to the folder
-				if (!System.IO.File.Exists (newFilePath)) {
-					Console.WriteLine (newFilePath + " Doesn't exist, we have to create it");
-					using (System.IO.StreamWriter file = new System.IO.StreamWriter(newFilePath)) {
-						file.WriteLine (textualData);
-					}
-				} else {
-					Console.WriteLine (newFilePath + " File Exists !, ReCreating it");
-					File.Delete (newFilePath);
-					using (System.IO.StreamWriter file = new System.IO.StreamWriter(newFilePath)) {
-						file.WriteLine (textualData);
-					}
+			// Check if the PDFDirectory Exists
+			if(!Directory.Exists(_pdfPath)){
+				Directory.CreateDirectory(_pdfPath);
+			}
+			else{
+				// Check if the IDCard is there, If Yes Delete It. Because we will download the fresh one just in a moment
+				if (File.Exists(_pdfFilePath)){
+					File.Delete(_pdfFilePath);
 				}
 			}
 
-			webView = FindViewById<WebView> (Resource.Id.webView1);
-			WebSettings settings = webView.Settings;
+			_webView = FindViewById<WebView> (Resource.Id.webView1);
+			var settings = _webView.Settings;
 			settings.JavaScriptEnabled = true;
 			settings.AllowFileAccessFromFileURLs = true;
 			settings.AllowUniversalAccessFromFileURLs = true;
 			settings.BuiltInZoomControls = true;
-			webView.SetWebChromeClient (new WebChromeClient ());
-			webView.LoadUrl("file:///" + pdfViewerPath + "/index.html");
+			_webView.SetWebChromeClient(new WebChromeClient());
+			_webView.LoadUrl("file:///android_asset/PDFViewer/index.html?file=" + _pdfFilePath);
 		}
 
 		protected override void OnResume ()
 		{
 			base.OnResume ();
-			webView.LoadUrl( "javascript:window.location.reload( true )" );
+			_webView.LoadUrl( "javascript:window.location.reload( true )" );
 		}
 
 		protected override void OnPause ()
 		{
 			base.OnPause ();
-			webView.ClearCache(true);
+			_webView.ClearCache(true);
 		}
 	}
 }
