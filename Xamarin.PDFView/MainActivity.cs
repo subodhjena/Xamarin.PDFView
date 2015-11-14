61d8804c65448011fc62cacf7daeb713ea6b3e48
Subodh Jena <jenasubodh@gmail.com>
2015-11-14 12:18:51 +0530

    Loads a pdf from URL by downloading it
    Loads a pdf from URL by downloading it.

--- Xamarin.PDFView/MainActivity.cs
+++ Xamarin.PDFView/MainActivity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Net;
 
 using Android.App;
 using Android.Content;
@@ -9,6 +10,8 @@ using Android.Widget;
 using Android.Webkit;
 using Android.OS;
 
+using AndroidHUD;
+
 namespace Xamarin.PDFView
 {
 	[Activity (Label = "PDFView", MainLauncher = true, Icon = "@drawable/icon")]
@@ -17,29 +20,17 @@ namespace Xamarin.PDFView
 	{
 		private  string _documentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
 		private  string _pdfPath;
-		private  string _pdfFileName = "ThePDFDocumentName.pdf";
+		private  string _pdfFileName = "thePDFDocument.pdf";
 		private  string _pdfFilePath;
 		private WebView _webView;
+		private string _pdfURL = @"http://sal.aalto.fi/publications/pdf-files/eluu11_public.pdf";
+		private WebClient _webClient = new WebClient();
 
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
-
-			_pdfPath = _documentsPath + "/PDFView";
-			_pdfFilePath  = Path.Combine(_pdfPath, _pdfFileName);
-
-			// Check if the PDFDirectory Exists
-			if(!Directory.Exists(_pdfPath)){
-				Directory.CreateDirectory(_pdfPath);
-			}
-			else{
-				// Check if the IDCard is there, If Yes Delete It. Because we will download the fresh one just in a moment
-				if (File.Exists(_pdfFilePath)){
-					File.Delete(_pdfFilePath);
-				}
-			}
 
 			_webView = FindViewById<WebView> (Resource.Id.webView1);
 			var settings = _webView.Settings;
@@ -48,7 +39,8 @@ namespace Xamarin.PDFView
 			settings.AllowUniversalAccessFromFileURLs = true;
 			settings.BuiltInZoomControls = true;
 			_webView.SetWebChromeClient(new WebChromeClient());
-			_webView.LoadUrl("file:///android_asset/PDFViewer/index.html?file=" + _pdfFilePath);
+
+			DownloadPDFDocument ();
 		}
 
 		protected override void OnResume ()
@@ -62,7 +54,49 @@ namespace Xamarin.PDFView
 			base.OnPause ();
 			_webView.ClearCache(true);
 		}
+
+		private void DownloadPDFDocument()
+		{
+			AndHUD.Shared.Show(this, "Downloading PDF\nPlease Wait ..", -1, MaskType.Clear);
+
+			_pdfPath = _documentsPath + "/PDFView";
+			_pdfFilePath  = Path.Combine(_pdfPath, _pdfFileName);
+
+			// Check if the PDFDirectory Exists
+			if(!Directory.Exists(_pdfPath)){
+				Directory.CreateDirectory(_pdfPath);
+			}
+			else{
+				// Check if the pdf is there, If Yes Delete It. Because we will download the fresh one just in a moment
+				if (File.Exists(_pdfFilePath)){
+					File.Delete(_pdfFilePath);
+				}
+			}
+
+			// This will be executed when the pdf download is completed
+			_webClient.DownloadDataCompleted += OnPDFDownloadCompleted;
+			// Lets downlaod the PDF Document
+			var url = new Uri(_pdfURL);
+			_webClient.DownloadDataAsync(url);
+		}
+
+		private void OnPDFDownloadCompleted (object sender, DownloadDataCompletedEventArgs e)
+		{
+			// Okay the download's done, Lets now save the data and reload the webview.
+			var pdfBytes = e.Result;
+			File.WriteAllBytes (_pdfFilePath, pdfBytes);
+
+			if(File.Exists(_pdfFilePath))
+			{
+				var bytes = File.ReadAllBytes(_pdfFilePath);
+			}
+
+			_webView.LoadUrl("file:///android_asset/PDFViewer/index.html?file=" + _pdfFilePath);
+
+			AndHUD.Shared.Dismiss();
+		}
 	}
+
 }
 
 
--- Xamarin.PDFView/Resources/Resource.designer.cs
+++ Xamarin.PDFView/Resources/Resource.designer.cs
@@ -26,10 +26,83 @@ namespace Xamarin.PDFView
 		
 		public static void UpdateIdValues()
 		{
+			global::AndroidHUD.Resource.Attribute.ahBarColor = global::Xamarin.PDFView.Resource.Attribute.ahBarColor;
+			global::AndroidHUD.Resource.Attribute.ahBarLength = global::Xamarin.PDFView.Resource.Attribute.ahBarLength;
+			global::AndroidHUD.Resource.Attribute.ahBarWidth = global::Xamarin.PDFView.Resource.Attribute.ahBarWidth;
+			global::AndroidHUD.Resource.Attribute.ahCircleColor = global::Xamarin.PDFView.Resource.Attribute.ahCircleColor;
+			global::AndroidHUD.Resource.Attribute.ahDelayMillis = global::Xamarin.PDFView.Resource.Attribute.ahDelayMillis;
+			global::AndroidHUD.Resource.Attribute.ahRadius = global::Xamarin.PDFView.Resource.Attribute.ahRadius;
+			global::AndroidHUD.Resource.Attribute.ahRimColor = global::Xamarin.PDFView.Resource.Attribute.ahRimColor;
+			global::AndroidHUD.Resource.Attribute.ahRimWidth = global::Xamarin.PDFView.Resource.Attribute.ahRimWidth;
+			global::AndroidHUD.Resource.Attribute.ahSpinSpeed = global::Xamarin.PDFView.Resource.Attribute.ahSpinSpeed;
+			global::AndroidHUD.Resource.Attribute.ahText = global::Xamarin.PDFView.Resource.Attribute.ahText;
+			global::AndroidHUD.Resource.Attribute.ahTextColor = global::Xamarin.PDFView.Resource.Attribute.ahTextColor;
+			global::AndroidHUD.Resource.Attribute.ahTextSize = global::Xamarin.PDFView.Resource.Attribute.ahTextSize;
+			global::AndroidHUD.Resource.Drawable.ic_errorstatus = global::Xamarin.PDFView.Resource.Drawable.ic_errorstatus;
+			global::AndroidHUD.Resource.Drawable.ic_successstatus = global::Xamarin.PDFView.Resource.Drawable.ic_successstatus;
+			global::AndroidHUD.Resource.Drawable.roundedbg = global::Xamarin.PDFView.Resource.Drawable.roundedbg;
+			global::AndroidHUD.Resource.Drawable.roundedbgdark = global::Xamarin.PDFView.Resource.Drawable.roundedbgdark;
+			global::AndroidHUD.Resource.Id.loadingImage = global::Xamarin.PDFView.Resource.Id.loadingImage;
+			global::AndroidHUD.Resource.Id.loadingProgressBar = global::Xamarin.PDFView.Resource.Id.loadingProgressBar;
+			global::AndroidHUD.Resource.Id.loadingProgressWheel = global::Xamarin.PDFView.Resource.Id.loadingProgressWheel;
+			global::AndroidHUD.Resource.Id.textViewStatus = global::Xamarin.PDFView.Resource.Id.textViewStatus;
+			global::AndroidHUD.Resource.Layout.loading = global::Xamarin.PDFView.Resource.Layout.loading;
+			global::AndroidHUD.Resource.Layout.loadingimage = global::Xamarin.PDFView.Resource.Layout.loadingimage;
+			global::AndroidHUD.Resource.Layout.loadingprogress = global::Xamarin.PDFView.Resource.Layout.loadingprogress;
+			global::AndroidHUD.Resource.String.library_name = global::Xamarin.PDFView.Resource.String.library_name;
+			global::AndroidHUD.Resource.Styleable.ProgressWheel = global::Xamarin.PDFView.Resource.Styleable.ProgressWheel;
+			global::AndroidHUD.Resource.Styleable.ProgressWheel_ahBarColor = global::Xamarin.PDFView.Resource.Styleable.ProgressWheel_ahBarColor;
+			global::AndroidHUD.Resource.Styleable.ProgressWheel_ahBarLength = global::Xamarin.PDFView.Resource.Styleable.ProgressWheel_ahBarLength;
+			global::AndroidHUD.Resource.Styleable.ProgressWheel_ahBarWidth = global::Xamarin.PDFView.Resource.Styleable.ProgressWheel_ahBarWidth;
+			global::AndroidHUD.Resource.Styleable.ProgressWheel_ahCircleColor = global::Xamarin.PDFView.Resource.Styleable.ProgressWheel_ahCircleColor;
+			global::AndroidHUD.Resource.Styleable.ProgressWheel_ahDelayMillis = global::Xamarin.PDFView.Resource.Styleable.ProgressWheel_ahDelayMillis;
+			global::AndroidHUD.Resource.Styleable.ProgressWheel_ahRadius = global::Xamarin.PDFView.Resource.Styleable.ProgressWheel_ahRadius;
+			global::AndroidHUD.Resource.Styleable.ProgressWheel_ahRimColor = global::Xamarin.PDFView.Resource.Styleable.ProgressWheel_ahRimColor;
+			global::AndroidHUD.Resource.Styleable.ProgressWheel_ahRimWidth = global::Xamarin.PDFView.Resource.Styleable.ProgressWheel_ahRimWidth;
+			global::AndroidHUD.Resource.Styleable.ProgressWheel_ahSpinSpeed = global::Xamarin.PDFView.Resource.Styleable.ProgressWheel_ahSpinSpeed;
+			global::AndroidHUD.Resource.Styleable.ProgressWheel_ahText = global::Xamarin.PDFView.Resource.Styleable.ProgressWheel_ahText;
+			global::AndroidHUD.Resource.Styleable.ProgressWheel_ahTextColor = global::Xamarin.PDFView.Resource.Styleable.ProgressWheel_ahTextColor;
+			global::AndroidHUD.Resource.Styleable.ProgressWheel_ahTextSize = global::Xamarin.PDFView.Resource.Styleable.ProgressWheel_ahTextSize;
 		}
 		
 		public partial class Attribute
 		{
+			
+			// aapt resource value: 0x7f010003
+			public const int ahBarColor = 2130771971;
+			
+			// aapt resource value: 0x7f01000b
+			public const int ahBarLength = 2130771979;
+			
+			// aapt resource value: 0x7f01000a
+			public const int ahBarWidth = 2130771978;
+			
+			// aapt resource value: 0x7f010008
+			public const int ahCircleColor = 2130771976;
+			
+			// aapt resource value: 0x7f010007
+			public const int ahDelayMillis = 2130771975;
+			
+			// aapt resource value: 0x7f010009
+			public const int ahRadius = 2130771977;
+			
+			// aapt resource value: 0x7f010004
+			public const int ahRimColor = 2130771972;
+			
+			// aapt resource value: 0x7f010005
+			public const int ahRimWidth = 2130771973;
+			
+			// aapt resource value: 0x7f010006
+			public const int ahSpinSpeed = 2130771974;
+			
+			// aapt resource value: 0x7f010000
+			public const int ahText = 2130771968;
+			
+			// aapt resource value: 0x7f010001
+			public const int ahTextColor = 2130771969;
+			
+			// aapt resource value: 0x7f010002
+			public const int ahTextSize = 2130771970;
 			
 			static Attribute()
 			{
@@ -45,7 +118,19 @@ namespace Xamarin.PDFView
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int ic_errorstatus = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int ic_successstatus = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int Icon = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int roundedbg = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int roundedbgdark = 2130837508;
 			
 			static Drawable()
 			{
@@ -60,8 +145,20 @@ namespace Xamarin.PDFView
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050002
+			public const int loadingImage = 2131034114;
+			
 			// aapt resource value: 0x7f050000
-			public const int webView1 = 2131034112;
+			public const int loadingProgressBar = 2131034112;
+			
+			// aapt resource value: 0x7f050003
+			public const int loadingProgressWheel = 2131034115;
+			
+			// aapt resource value: 0x7f050001
+			public const int textViewStatus = 2131034113;
+			
+			// aapt resource value: 0x7f050004
+			public const int webView1 = 2131034116;
 			
 			static Id()
 			{
@@ -77,7 +174,16 @@ namespace Xamarin.PDFView
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			public const int loading = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int loadingimage = 2130903041;
+			
+			// aapt resource value: 0x7f030002
+			public const int loadingprogress = 2130903042;
+			
+			// aapt resource value: 0x7f030003
+			public const int Main = 2130903043;
 			
 			static Layout()
 			{
@@ -92,11 +198,14 @@ namespace Xamarin.PDFView
 		public partial class String
 		{
 			
+			// aapt resource value: 0x7f040002
+			public const int app_name = 2130968578;
+			
 			// aapt resource value: 0x7f040001
-			public const int app_name = 2130968577;
+			public const int hello = 2130968577;
 			
 			// aapt resource value: 0x7f040000
-			public const int hello = 2130968576;
+			public const int library_name = 2130968576;
 			
 			static String()
 			{
@@ -104,6 +213,70 @@ namespace Xamarin.PDFView
 			}
 			
 			private String()
+			{
+			}
+		}
+		
+		public partial class Styleable
+		{
+			
+			public static int[] ProgressWheel = new int[]
+			{
+					2130771968,
+					2130771969,
+					2130771970,
+					2130771971,
+					2130771972,
+					2130771973,
+					2130771974,
+					2130771975,
+					2130771976,
+					2130771977,
+					2130771978,
+					2130771979};
+			
+			// aapt resource value: 3
+			public const int ProgressWheel_ahBarColor = 3;
+			
+			// aapt resource value: 11
+			public const int ProgressWheel_ahBarLength = 11;
+			
+			// aapt resource value: 10
+			public const int ProgressWheel_ahBarWidth = 10;
+			
+			// aapt resource value: 8
+			public const int ProgressWheel_ahCircleColor = 8;
+			
+			// aapt resource value: 7
+			public const int ProgressWheel_ahDelayMillis = 7;
+			
+			// aapt resource value: 9
+			public const int ProgressWheel_ahRadius = 9;
+			
+			// aapt resource value: 4
+			public const int ProgressWheel_ahRimColor = 4;
+			
+			// aapt resource value: 5
+			public const int ProgressWheel_ahRimWidth = 5;
+			
+			// aapt resource value: 6
+			public const int ProgressWheel_ahSpinSpeed = 6;
+			
+			// aapt resource value: 0
+			public const int ProgressWheel_ahText = 0;
+			
+			// aapt resource value: 1
+			public const int ProgressWheel_ahTextColor = 1;
+			
+			// aapt resource value: 2
+			public const int ProgressWheel_ahTextSize = 2;
+			
+			static Styleable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Styleable()
 			{
 			}
 		}
